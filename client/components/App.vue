f9b38433ec2d807b9c6f2041077a5a336de06195
Drew Lustro <drewlustro@gmail.com>
2017-03-22 18:48:09 -0400

    Add simple logos; favicon; alignment / lorem ipsum

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,27 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <nav>
+      <router-link :to="{ name: 'home' }"><img src="/images/logo-rect.png" class="logo-rect"></router-link>
+    </nav>
+    <section class="content">
+      <router-view></router-view>
+    </section>
   </div>
 </template>
 
-<style>
-body {
-  margin: 0;
-  font-size: 2rem;
-  font-family: -apple-system, BlinkMacSystemFont,
-               'avenir next', avenir,
-               helvetica, 'helvetica neue',
-               Ubuntu,
-               'segoe ui', arial,
-               sans-serif;
-}
-.page {
-  text-align: center;
-  /* nesting for the need to test postcss */
-  code {
-    background-color: #f0f0f0;
-    padding: 3px 5px;
-    border-radius: 2px;
-  }
-}
+<style lang="sass">
+body
+  margin: 0
+  font-size: 1.2rem
+  font-family: -apple-system, BlinkMacSystemFont, 'avenir next', avenir, helvetica, 'helvetica neue', Ubuntu, 'segoe ui', arial, sans-serif
+
+img
+  max-width: 100%
+
+nav
+  max-width: 1080px
+  margin-left: auto
+  margin-right: auto
+  margin-bottom: 2em
+  padding: 1em 0
+
+section.content
+  max-width: 1080px
+  margin-left: auto
+  margin-right: auto
+
+.logo-rect
+  max-width: 222px
+
 </style>
--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="page">
-    <h1>Periodic Audio</h1>
+    <p>
+      Donec finibus magna vitae sem consequat luctus. Donec id risus in velit iaculis pharetra vel malesuada neque. Phasellus blandit nisi non tincidunt accumsan. Pellentesque sem enim, tincidunt et maximus sed, aliquam sit amet erat. Donec aliquam, lacus in finibus vulputate, purus lorem scelerisque ex, vel fermentum lectus justo sed arcu. Etiam sollicitudin metus eget tempus placerat. Sed luctus porttitor risus quis consequat. Quisque convallis vehicula tortor vulputate tempor. Cras at massa id ex rhoncus rutrum. Sed aliquam fringilla purus, at ornare arcu ultrices ut. Donec felis mi, condimentum in ante nec, convallis imperdiet dolor.
+    </p>
+    <p>
+      Curabitur finibus justo nec pretium interdum. Duis elementum feugiat urna, iaculis porttitor arcu commodo et. Pellentesque dui lorem, porta eu odio at, posuere fermentum magna. Maecenas dapibus quis ipsum eu venenatis. Aliquam ac enim vitae leo venenatis feugiat sed vel magna. In sed tempus risus, quis ultrices eros. Ut efficitur ex pharetra hendrerit mattis. Mauris tempus massa sit amet magna vehicula accumsan. Sed eu est ut velit convallis congue vel at dui. Mauris ac justo lacus. Phasellus sit amet vulputate nibh, eu rutrum nibh. Donec a viverra sapien, at sodales ante. Morbi rutrum maximus lorem, id faucibus nisi pharetra at. Duis fringilla vel mauris vitae placerat. Donec laoreet hendrerit ipsum et lobortis. Mauris ornare eros a lobortis convallis.
+    </p>
   </div>
 </template>
 
